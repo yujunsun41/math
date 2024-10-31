332c4788f184f47528bef0cff659a4666240f5c4
yujunsun41 <gimpo.s230215@ggh.goe.go.kr>
2024-10-31 15:32:24 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -10,13 +10,15 @@ st.write('음수?? 어렵지않아!')
 
 # 여러 개의 열(문단)을 생성
 #col1, col2 = st.columns(2)       
-# 왼쪽 문단#with col1:
+# 왼쪽 문단
+with col1:
       st.subheader('**개념정리')
       st.write('- 양수: 0보다 큰 수')
       st.write('- 음수: 0보다 작은 수')
       st.write('- 양수: (+) / 음수: (-) 부호가 붙음')
       st.write('- 부호: (+)는 생략 가능 /(-)는 생략 불가')
-# 오른쪽 문단#with col2:
+# 오른쪽 문단
+with col2:
       st.image('수학이미지.PNG')  # 이미지 파일 불러오기
 
 # 사용자의 입력을 받아서 a에 저장하기(초기값은 0)
